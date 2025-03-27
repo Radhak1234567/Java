3dc45448612f31d7d88443886db0ceeb60b36c57
radha <radhahosamani123@gmail.com>
2025-03-27 21:33:48 +0530

    add

--- has a relationship march 27/projectorr/src/com/xworkz/projectorr/components/Port.java
+++ has a relationship march 27/projectorr/src/com/xworkz/projectorr/components/Port.java
@@ -3,22 +3,20 @@ package com.xworkz.projectorr.components;
 public class Port {
     private String portType; // Private variable
 
-    // Constructor
+
     public Port(String portType) {
         this.portType = portType;
     }
 
-    // Getter Method
+
     public String getPortType() {
         return portType;
     }
 
-    // ✅ Setter Method (Important!)
     public void setPortType(String portType) {
         this.portType = portType;
     }
 
-    // Display Method
     public void display() {
         System.out.println("Port Type: " + portType);
     }
